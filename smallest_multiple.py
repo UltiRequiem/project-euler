2081944eb5659dadb788bc42d176e7be54cd6ccf
Eliaz Bobadilla <ultirequiem@pm.me>
2024-03-02 19:55:24 -0500

    chicken time

---
+++ smallest_multiple.py
@@ -0,0 +1,18 @@
+result = 1
+
+# BRO IT SAYS THE SMALLER ONE
+# learn how to read lmao
+"""
+for number in range(1, 11):
+    print(f'{result} x {number} = {result*number}')
+    result = result * number
+
+print(result)
+"""
+
+"""
+actually this is far easier with just a little bit of paper:
+232792560, see smallest_multiple.png
+
+work on a proper function when you got time
+"""
